fa5fff43650dc3838575a5b8cb0f33f4ffd78ce0
i-amit88 <102894134+i-amit88@users.noreply.github.com>
2022-08-25 00:33:17 +0530

    Time: 37 ms (63.41%), Space: 17.8 MB (42.69%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -3,7 +3,7 @@ public:
     
     void help(int i ,int n , int k , vector<int> &subset , vector<vector<int>> &ans){
         //if(i > n){
-            if(subset.size()==k){
+            if(k==0){
                 ans.push_back(subset);
                 return;
             }
@@ -12,7 +12,7 @@ public:
         
         
         subset.push_back(i);   //take the element
-        help(i+1 , n , k , subset , ans);
+        help(i+1 , n , k-1 , subset , ans);
         
         //skip the element
         subset.pop_back();
